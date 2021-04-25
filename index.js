2f361523b04ebe31f004d3167d358787b13fe098
Godric2003D <66315809+Godric2003D@users.noreply.github.com>
2021-04-26 03:00:29 +0530

    first

---
+++ index.js
@@ -0,0 +1,84 @@
+function audio1(){
+    var dor=document.getElementById("a");
+    dor.play();
+}
+function w(event){
+    var dora = document.getElementById("a");
+    var gg=event.key;
+    
+    if(gg === 'w'){
+        dora.play();
+    }
+}
+function audio2(){
+    var dor = document.getElementById("b");
+    dor.play();
+}
+function a(event){
+    var dora = document.getElementById("b");
+    var g=event.key;
+    
+    if(g === 'a'){
+        dora.play();
+    }
+}
+function audio3(){
+    var doraa = document.getElementById("c");
+    doraa.play();
+}
+function s(event){
+    var dora = document.getElementById("c");
+    var zz=event.key;
+    
+    if(zz === 's'){
+        dora.play();
+    }
+}
+function audio4(){
+    var doraa = document.getElementById("d");
+    doraa.play();
+}
+function d(event){
+    var doraaa = document.getElementById("d");
+    var zzz=event.key;
+    
+    if(zzz === 'd'){
+        dora.play();
+    }
+}
+function audio5(){
+    var doraa = document.getElementById("e");
+    doraa.play();
+}
+function j(event){
+    var doraaa = document.getElementById("e");
+    var zzzz=event.key;
+    
+    if(zzzz === 'j'){
+        doraaa.play();
+    }
+}
+function audio6(){
+    var doraa = document.getElementById("f");
+    doraa.play();
+}
+function k(event){
+    var doraaa = document.getElementById("f");
+    var zzzzz=event.key;
+    
+    if(zzzzz === 'k'){
+        doraaa.play();
+    }
+}
+function audio7(){
+    var doraa = document.getElementById("g");
+    doraa.play();
+}
+function l(event){
+    var doraaa = document.getElementById("g");
+    var zzzzzz=event.key;
+    
+    if(zzzzzz === 'l'){
+        doraaa.play();
+    }
+}
